c05b4c743d75f05e03b3965c4822a8363e692f5b
Koezingone <k.ilham0601@gmail.com>
2024-06-11 13:54:26 +0700

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -3,5 +3,13 @@ package hitung;
 
 
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        //membuat objek
+        Matematika ilham = new Matematika(8,2);
+        
+        System.out.println("Hasil Penjumlahan : " + ilham.setPenjumlahan());
+        System.out.println("Hasil Pengurangan : " + ilham.setPengurangan());
+        System.out.println("Hasil Perkalian : " + ilham.setPerkalian());
+        System.out.println("Hasil Pembagian : " + ilham.setPembagian());
+    }
 }
